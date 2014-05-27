4b8096bfcea87d2925591ecdc78be81220352935
fatuk <fatukk@gmail.com>
2014-05-27 19:11:36 +0400

    First commit

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(function () {
+	window.App = {};
+	window.App.Views = {};
+	window.App.Models = {};
+	window.App.Collection = {};
+
+	App.Views.App = Backbone.View.extend({
+
+	});
+
+	App.Views.Step = Backbone.View.extend({
+
+	});
+});
